d153d9f61f27fc0dc0670294de041666c58e8d78
Bogdan Putintsev <bogdan.putintsev@post.cz>
2021-11-20 15:45:28 +0700

    Lesson 26

---
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -0,0 +1,49 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#include "Player/STUBaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ASTUBaseCharacter::ASTUBaseCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->bUsePawnControlRotation = true;
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+    CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void ASTUBaseCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    check(GetCharacterMovement());
+    check(GetCapsuleComponent())
+}
+
+void ASTUBaseCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &ASTUBaseCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("LookUp", this, &ASTUBaseCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("TurnAround", this, &ASTUBaseCharacter::AddControllerYawInput);
+}
+
+void ASTUBaseCharacter::MoveForward(float Amount)
+{
+    AddMovementInput(GetActorForwardVector(), Amount);
+}
+
+void ASTUBaseCharacter::MoveRight(float Amount)
+{
+    AddMovementInput(GetActorRightVector(), Amount);
+}
---
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -0,0 +1,35 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "STUBaseCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+  public:
+    ASTUBaseCharacter();
+
+  protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USpringArmComponent *SpringArmComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UCameraComponent *CameraComponent;
+
+    virtual void BeginPlay() override;
+
+  public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+  
+private:
+    void MoveForward(float Amount);
+    void MoveRight(float Amount);
+};
